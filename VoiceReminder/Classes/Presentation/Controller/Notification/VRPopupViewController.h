fd7451bcae564888f8e14df406fdeac9044d705f
diep nguyen <anhdbk@gmail.com>
2015-06-23 21:38:28 +0700

    update popupview

---
+++ VoiceReminder/Classes/Presentation/Controller/Notification/VRPopupViewController.h
@@ -0,0 +1,13 @@
+//
+//  VRPopupViewController.h
+//  VoiceReminder
+//
+//  Created by Điệp Nguyễn on 6/23/15.
+//  Copyright (c) 2015 Owner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VRPopupViewController : UIViewController
+
+@end
---
+++ VoiceReminder/Classes/Presentation/Controller/Notification/VRPopupViewController.m
@@ -0,0 +1,37 @@
+//
+//  VRPopupViewController.m
+//  VoiceReminder
+//
+//  Created by Điệp Nguyễn on 6/23/15.
+//  Copyright (c) 2015 Owner. All rights reserved.
+//
+
+#import "VRPopupViewController.h"
+
+@interface VRPopupViewController ()
+
+@end
+
+@implementation VRPopupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
